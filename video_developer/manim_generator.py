9c605ba1b976af4473ba8f6e77c31168379267ec
Tolulope <tolulopee90@gmail.com>
2026-05-10 23:38:16 +0100

    turn tutorial script to a manim code

--- video_developer/manim_generator.py
+++ video_developer/manim_generator.py
@@ -1,6 +1,8 @@
 from groq import Groq
 
 
+client = Groq()
+
 SYSTEMS_PROMPT = """you are a professional video designer, your job is to take a script and transform it to a manim code, using interactive and intuitive 
                     designs, ensure that the objects do not overlap, you can make use of images, 3d materials, backgrounds, foregrounds (referrred to as assets),... 
                     you can get a list of avaialble asset and what they represent using a tool call, if an asset in needed but not available in the existing assets,
@@ -10,4 +12,30 @@ SYSTEMS_PROMPT = """you are a professional video designer, your job is to take a
 
 
 
-def 
+def generate_code(script:list):
+
+    codes = []
+
+    for scene in script:
+        time_frame, content = scene
+        
+        messages = [
+            {
+                "role": "system",
+                "content": SYSTEMS_PROMPT
+            },
+            {
+                "role": "user",
+                "content": f"for the time frame {time_frame} create the manim code for this content {content}"
+
+            }
+        ],
+
+        model = model
+        completion = client.chat.completions.create(
+            messages=messages,
+        model="llama-3.3-70b-versatile", 
+        temperature=0.7,
+        top_p=1
+        )
+        return completions[0].message.content
